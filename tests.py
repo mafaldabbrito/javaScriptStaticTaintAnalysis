6401d78518741b7cca9b4cca9b151df8c78bfe49
mafaldabbrito <mafabrito@gmail.com>
2025-05-16 21:18:48 +0200

    new tests

--- tests.py
+++ tests.py
@@ -16,49 +16,27 @@ if __name__ == "__main__":
 
    # patterns = [dom_xss]
    # policy = Policy(patterns)
-
-   # # Simulate a program
-   # multilabel = MultiLabel(patterns)
-   # multilabel.add_source("DOM XSS", "document.URL")
-   # multilabel.add_sanitizer("DOM XSS",  "document.URL", "DOMPurify.sanitize")  # <- Comment this line to test unsanitized
-
-   # # Track variable flows
-   # env = MultiLabelling()
-   # env.set_multilabel("output", multilabel)
-
-   # # Check for illegal flow
-   # illegal_flows = policy.detect_illegal_flows("document.write", env.get_multilabel("output"))
-
-   # # Report
-   # vulnerabilities = Vulnerabilities()
-   # source_positions = {
-   #     "document.URL": 1,
-   #     "DOMPurify.sanitize": 3
-   # }
-   # sink_position = 4
-
-   # vulnerabilities.add_illegal_flow("document.write", illegal_flows, source_positions, sink_position)
-
-   # print("=== Detected Vulnerabilities ===")
-   # for v in vulnerabilities.get_all():
-   #     print(v)
-
-   # vulnerabilities.export_json("vulnerabilities_report.json")
-   # print("\nReport saved to vulnerabilities_report.json")
-
-   # Test AST Parser
+   # Load patterns and slices
    from utils.ast_parser import Parser
    from utils.ast_traversal import TraversalVisitor
-   parser=Parser("test.js")
-   ast = parser.parse_js_code()
-   # if ast_tree:
-   #     print("AST Tree:")
-   #     print(ast_tree)
-   # else:
-   #     print("Failed to parse the JavaScript code.")
+   from utils.load_patterns_and_slices import load_pattern_and_slice
+   pattern, ast = load_pattern_and_slice("1a-basic-flow.patterns.json", "1a-basic-flow.js")
+   policy = Policy(pattern)
+   
+   # Simulate a program
+   multilabel = MultiLabel(pattern)
+  
+   
    # Test AST Traversal
-   visitor = TraversalVisitor()
+   visitor = TraversalVisitor(policy)
    visitor.visit(ast)  # ast is the top-level Program node
    print("=== Initialized Variables ===")
    for var in visitor.get_initialized_vars():
       print(f"Variable: {var[0]}, Line: {var[1]}")
+
+   print("=== Vulnerabilities ===")
+   for vuln in visitor.get_vulnerabilities():
+      print(vuln)
+   
+   # Export vulnerabilities to JSON
+   visitor.get_output("vulnerabilities.json")
